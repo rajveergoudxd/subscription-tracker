2d2f1a4abea99e394726be4e3adaee7496d8ca74
Rajveer Goud <goudrajveer8@gmail.com>
2025-11-10 18:48:56 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import { PORT } from './config/env.js';
+import userRouter from './routes/user.router.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDatabase from './database/mongodb.js';
+const app = express();
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the subscription tracker api.");
+});
+
+app.listen(5500, async () => {
+    console.log(`Subscription Tracker API is running on http://localhost:${PORT}/`);
+
+    await connectToDatabase();
+});
+
+export default app;
---
+++ middlewares/error.middleware.js
@@ -0,0 +1,27 @@
+const errorMiddleware = (err, req, res, next) => {
+    try {
+        let error = { ...err };
+
+        error.message = err.message;
+
+        console.error(err);
+
+        // Mongoose bad ObjectId
+        if (err.name === 'CastError') {
+            const message = 'Resource not found';
+            error = new Error(message);
+            error.statusCode = 404;
+        }
+
+        // Mongoose duplicate Key
+        if (err.code === 11000) {
+            const message = 'Duplicate field value entered';
+            error = new Error(message);
+            error.statusCode = 400;
+        }
+
+        // Mongoose Validation error
+    } catch(error) {
+        next(error);
+    }
+}
