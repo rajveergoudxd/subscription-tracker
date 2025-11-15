49c2fe040edb6fe0eed2e5c1c30edd4da3c12162
Rajveer Goud <goudrajveer8@gmail.com>
2025-11-15 21:26:43 +0530

    auth logic updated

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -8,42 +8,34 @@ import { JWT_EXPIRES_IN, JWT_SECRET } from "../config/env.js";
 
 export const signUp = async (req, res, next) => {
     // Implement sign up logic here
-    console.log("Starting the session and transaction.")
     const session = await mongoose.startSession();
     session.startTransaction();
-
-    console.log("Started Transaction Successfully.")
 
     try {
         // Create a new user
         const { name, email, password } = req.body;
-        console.log("Let's create a new user.")
+        
         // Check if a user already exists
         const existingUser = await User.findOne({email}).session(session);
-        console.log("Let's check and validate if this user already exists.")
+        
         if (existingUser) {
             const error = new Error('User already exists');
             error.statusCode = 409;
             throw error;
         }
 
-        console.log("let's hash the password")
         // Hash password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        console.log("let's create this user finally")
         const newUsers = await User.create([{name, email, password: hashedPassword}], { session });
 
-        console.log("Create an JWT token")
         const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
 
-        console.log("Everything went fine.")
         await session.commitTransaction();
-        console.log("Transaction Committed")
         session.endSession();
-        console.log("Session ended")
-        return res.status(201).json({
+        
+        res.status(201).json({
             success: true,
             message: 'User created successfully',
             data: {
@@ -60,6 +52,38 @@ export const signUp = async (req, res, next) => {
 
 export const signIn = async (req, res, next) => {
     // Implement sign in logic here
+    try {
+        const { email, password } = req.body;
+    
+        const user = await User.findOne({ email });
+    
+        if (!user) {
+            const error = new Error('User not found');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            const error = new Error('Invalid Password');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+
+        res.status(200).json({
+            success: true,
+            message: 'User signed in successfully',
+            data: {
+                token,
+                user
+            }
+        })
+    } catch (error) {
+        next(error);
+    }
 }
 
 export const signOut = async (req, res, next) => {
